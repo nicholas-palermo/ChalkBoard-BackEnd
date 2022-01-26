392769544cb6ca53081fad999784cafcfa7cd97e
Nick Palermo <nicholas.palermo8@gmail.com>
2022-01-26 11:00:34 -0500

    updated routes

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ app.use(express.json()); // => allows us to access to the req.body
 
     app.post("/signup", async(req, res) => {
         try {
-            const { studentID, fname, lname, email, password } = req.body;
+            const { userType, fname, lname, email, pass } = req.body;
             console.log(req.body);
-            const newStudent = await pool.query("INSERT INTO student (studentID, fname, lname, email, pass) VALUES ($1, $2, $3, $4, $5) RETURNING *", 
-            [studentID, fname, lname, email, password]);
+            const newStudent = await pool.query("INSERT INTO student (userType, fname, lname, email, pass) VALUES ($1, $2, $3, $4, $5) RETURNING *", 
+            [userType, fname, lname, email, pass]);
 
             res.json(newStudent.rows[0]);
         } catch (err) {
@@ -29,12 +29,12 @@ app.use(express.json()); // => allows us to access to the req.body
 
     //create a faculty member
 
-    app.post("/signup", async(req, res) => {
+    app.post("/signup/faculty", async(req, res) => {
         try {
-            const { facultyID, fname, lname, email, password } = req.body;
+            const { userType, fname, lname, email, pass } = req.body;
             console.log(req.body);
-            const newFaculty = await pool.query("INSERT INTO faculty (facultyID, fname, lname, email, pass) VALUES ($1, $2, $3, $4, $5) RETURNING *", 
-            [facultyID, fname, lname, email, password]);
+            const newFaculty = await pool.query("INSERT INTO faculty (userType, fname, lname, email, pass) VALUES ($1, $2, $3, $4, $5) RETURNING *", 
+            [userType, fname, lname, email, pass]);
 
             res.json(newFaculty.rows[0]);
         } catch (err) {
@@ -44,12 +44,12 @@ app.use(express.json()); // => allows us to access to the req.body
 
     //create a course
 
-    app.post("/", async(req, res) => {
+    app.post("/newCourse", async(req, res) => {
         try {
-            const { courseID, courseName, facultyID, term, subject } = req.body;
+            const { courseID, courseName, facultyID, term, courseSubject } = req.body;
             console.log(req.body);
-            const newCourse = await pool.query("INSERT INTO Course (courseID, courseName, faculty, term, subject) VALUES ($1, $2, $3, $4, $5) RETURNING *", 
-            [courseID, courseName, facultyID, term, subject]);
+            const newCourse = await pool.query("INSERT INTO Course (courseID, courseName, facultyID, term, courseSubject) VALUES ($1, $2, $3, $4, $5) RETURNING *", 
+            [courseID, courseName, facultyID, term, courseSubject]);
 
             res.json(newCourse.rows[0]);
         } catch (err) {
@@ -59,12 +59,12 @@ app.use(express.json()); // => allows us to access to the req.body
 
     //create an assignment
 
-    app.post("/:courseID/assignments", async(req, res) => {
+    app.post("/:courseID/newAssignment", async(req, res) => {
         try {
-            const { assignmentID, courseID, title, description, dateAssigned, dateDue, maxGrade } = req.body;
+            const { courseID, term, title, description, maxGrade, dateAssigned, dateDue } = req.body;
             console.log(req.body);
-            const newAssignment = await pool.query("INSERT INTO Assignment (assignmentID, courseID, title, description, dateAssigned, dateDue, maxGrade) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *", 
-            [assignmentID, courseID, title, description, dateAssigned, dateDue, maxGrade]);
+            const newAssignment = await pool.query("INSERT INTO Assignment (courseID, term, title, description, maxGrade, dateAssigned, dateDue) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *", 
+            [courseID, term, title, description, maxGrade, dateAssigned, dateDue]);
 
             res.json(newAssignment.rows[0]);
         } catch (err) {
@@ -104,7 +104,80 @@ app.use(express.json()); // => allows us to access to the req.body
 
 //READ
 
-    //
+    //get a student during login
+    app.get("/login/:email"), async(req,res) => {
+        try {
+            const { email } = req.params;
+            const student = await pool.query ("SELECT * FROM Student WHERE email = $1", [email])
+
+            res.json(student.rows[0]);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+
+    //get a faculty member
+    app.get("/:facultyID/"), async(req,res) => {
+        try {
+            const {facultyID} = req.params;
+            const faculty = await pool.query ("SELECT * FROM Faculty WHERE facultyID = $1", [facultyID])
+
+            res.json(faculty.rows[0]);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    //get a student's courses
+    app.get("/:studentID/"), async(req, res) => {
+        try {
+            const { studentID } = req.params;
+            const studentCourses = await pool.query("SELECT * FROM StudentCourse WHERE studentID = $1", [studentID]);
+
+            res.json(studentCourses.rows);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+/*
+    //get a course
+    app.get(), async(req, res) => {
+        try {
+           
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+*/
+    //get a courses assignment's
+     app.get("/Assignments/:courseID", async(req, res) => {
+        try {
+            const {courseID} = req.params;
+            const courseAssgignments = await pool.query("SELECT * FROM Assignment WHERE courseID = $1", [courseID]);
+
+            res.json(courseAssgignments.rows);
+        } catch (err) {
+            console.error(err.message);
+        }
+    })
+
+    
+    //get an assignment
+   
+
+
+    //get a student's grades
+    app.get("/Grades"), async(req, res) => {
+        try {
+           
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
 
 
 //UPDATE
